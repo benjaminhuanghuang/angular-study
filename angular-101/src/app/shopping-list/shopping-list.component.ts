e99682605b11365d14d748c6573201a4de257f44
benjaminhuanghuang <bhuang@rms.com>
2022-05-05 23:17:57 -0700

    recipe model

--- angular-101/src/app/shopping-list/shopping-list.component.ts
+++ angular-101/src/app/shopping-list/shopping-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+  recipes = [];
   constructor() { }
 
   ngOnInit(): void {
