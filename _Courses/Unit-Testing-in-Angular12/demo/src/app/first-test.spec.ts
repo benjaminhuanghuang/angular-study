78c153dc802263fe84c45319cd1c711beda89e23
BenjaminHuang <benjaminhuanghuang@gmail.com>
2022-08-26 18:55:46 -0700

    update

---
+++ _Courses/Unit-Testing-in-Angular12/demo/src/app/first-test.spec.ts
@@ -0,0 +1,17 @@
+//
+describe("My first test", () => {
+  let sut;
+
+  beforeEach(() => {
+    sut = {}
+  })
+
+  it("", () => {
+    // arrange
+    sut.a = false;
+    // act
+
+    // assert
+    expect(sut.a).toBe(false);
+  })
+})
