dac1e67e225dce9a9c5bd0f5efd4c2a4c8d16d2a
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-10-13 22:35:23 -0700

    segment digits

--- Ng16-Standalone/Introduction to Angular v16 Standalone Components and Signals/seven-segment/src/app/number-display-test/number-display-test.component.ts
+++ Ng16-Standalone/Introduction to Angular v16 Standalone Components and Signals/seven-segment/src/app/number-display-test/number-display-test.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RegularNumberComponent } from '../regular-number/regular-number.component';
 import { SevenSegmentNumberComponent } from '../seven-segment-number/seven-segment-number.component';
 
 @Component({
   selector: 'app-number-display-test',
   standalone: true,
-  imports: [CommonModule, RegularNumberComponent, SevenSegmentNumberComponent],
+  imports: [CommonModule, RegularNumberComponent, SevenSegmentNumberComponent, ReactiveFormsModule],
   templateUrl: './number-display-test.component.html',
   styleUrls: ['./number-display-test.component.scss']
 })
 export class NumberDisplayTestComponent {
-
+  numberInput = new FormControl(0);
+  numberOfDigitsInput = new FormControl(4);
+  precision = new FormControl(2);
+  sixteenSegmentsDisplayText = new FormControl('');
 }
