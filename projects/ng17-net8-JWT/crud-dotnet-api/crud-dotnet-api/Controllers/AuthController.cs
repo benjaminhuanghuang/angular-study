faf6d75ce1b8700eb675048f2e45080fbc03e920
BenjaminHuang <benjaminhuanghuang@gmail.com>
2024-01-15 18:30:34 -0800

    update

---
+++ projects/ng17-net8-JWT/crud-dotnet-api/crud-dotnet-api/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace crud_dotnet_api.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
