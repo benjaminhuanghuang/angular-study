ad88434d9c37af8c0d4ce170ac2de5b161fc6903
BenjaminHuang <benjaminhuanghuang@gmail.com>
2024-05-22 22:41:30 -0700

    register

--- projects/angular17-net8-JWT-auth/API/Controllers/AccountController.cs
+++ projects/angular17-net8-JWT-auth/API/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-//using API.Dtos;
+using API.Dtos;
 using API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -16,9 +16,62 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
-        // private readonly UserManager<AppUser> _userManager;
-        // private readonly RoleManager<IdentityRole> _roleManager;
-        // private readonly IConfiguration _configuration;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        public AccountController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<string>> Register(RegisterDto registerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new AppUser
+            {
+                Email = registerDto.Email,
+                FullName = registerDto.FullName,
+                UserName = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            if (registerDto.Roles is null)
+            {
+                await _userManager.AddToRoleAsync(user, "User");
+            }
+            else
+            {
+                foreach (var role in registerDto.Roles)
+                {
+                    await _userManager.AddToRoleAsync(user, role);
+                }
+            }
+
+
+            return Ok(new AuthResponseDto
+            {
+                IsSuccess = true,
+                Message = "Account Created Successfully!"
+            });
+
+        }
+
         [HttpGet]
         public string Hi()
         {
